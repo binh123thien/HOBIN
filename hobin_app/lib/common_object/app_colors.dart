428ff384bd5f802ee26bd5bf69bb2e2d19cccece
DoanHoc <doanhoc172@gmail.com>
2023-02-01 20:27:42 +0700

    ss

--- hobin_app/lib/common_object/app_colors.dart
+++ hobin_app/lib/common_object/app_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color purpleColor = Color(0xff940DC6);
-  // static const Color mainBlueColor = Color(0xff005BE0);
+  static const Color grayClolor = Color(0xffD9D9D9);
   // static const Color blueDarkColor = Color(0xff252B5C);
   // static const Color textColor = Color(0xff53587A);
   // static const Color greyColor = Color(0xffAAAAAA);
